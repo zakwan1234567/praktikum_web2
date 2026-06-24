53f547cd6d6bafc9e4670b32a523cd23699d4410
zakwan1234567 <zakwankolaka@gmail.com>
2026-06-24 14:24:01 +0700

    Usu

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PharIo\Manifest\Email;
 
-#[Fillable(['name', 'email', 'password'])]
+// #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
